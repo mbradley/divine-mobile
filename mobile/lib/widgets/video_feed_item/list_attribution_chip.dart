15876280e6ae4560fd5c96481fc04f39a21c173c
rabble <evan@protest.net>
2026-01-03 23:48:10 +1300

    fix: use withValues instead of deprecated withOpacity

--- mobile/lib/widgets/video_feed_item/list_attribution_chip.dart
+++ mobile/lib/widgets/video_feed_item/list_attribution_chip.dart
@@ -53,7 +53,7 @@ class ListAttributionChip extends StatelessWidget {
             decoration: BoxDecoration(
               color: VineTheme.cardBackground,
               borderRadius: BorderRadius.circular(12),
-              border: Border.all(color: VineTheme.vineGreen.withOpacity(0.5)),
+              border: Border.all(color: VineTheme.vineGreen.withValues(alpha: 0.5)),
             ),
             child: Row(
               mainAxisSize: MainAxisSize.min,
